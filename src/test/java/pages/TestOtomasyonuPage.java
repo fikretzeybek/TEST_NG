cd1f38251fcc598df41dd453faad1f908015335d
fikretzeybek <fzeybek1972@gmail.com>
2023-12-06 20:14:30 +0300

    t_ng

--- src/test/java/pages/TestOtomasyonuPage.java
+++ src/test/java/pages/TestOtomasyonuPage.java
@@ -1,6 +1,5 @@
 package pages;
 
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -21,5 +20,23 @@ public class TestOtomasyonuPage {
 
     @FindBy(xpath = "//div[@class= 'product-box my-2  py-1']")
     public List<WebElement> bulunanUrunElementleriList;
+
+    @FindBy(xpath = "//div[@class=' heading-sm mb-4']")
+    public WebElement urunSayfasindakiUrunIsimElementi;
+
+    @FindBy(xpath = "(//a[@class='e-cart'])[1]")
+    public WebElement accountLinki;
+
+    @FindBy(xpath = "//*[@id='email']")
+    public WebElement emailKutusu;
+
+    @FindBy(id = "password")
+    public WebElement passwordKutusu;
+
+    @FindBy(id = "submitlogin")
+    public WebElement loginButonu;
+
+    @FindBy(xpath = "//span[text()='Logout']")
+    public WebElement logoutLinki;
 
 }
